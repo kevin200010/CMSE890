a7d2e76fdef5c3106a9c75ff0570f9ffc337c2c0
kevin200010 <kevin18patel@gmail.com>
2023-04-22 00:54:01 -0400

    streamlit app creation

--- streamlit.py
+++ streamlit.py
@@ -1,9 +1,10 @@
-import streamlit as st
-from tensorflow import keras
-from keras.preprocessing import image, sequence
 import numpy as np
 import pickle
 import tensorflow as tf
+import streamlit as st
+from tensorflow import keras
+
+
 
 
 # Read dictionary pkl file
@@ -19,12 +20,10 @@ embedding_size = 128
 max_len = 40
 
 from PIL import Image
-
+import cv2
 def preprocessing(img_path):
-    im = Image.open("C:\\Users\\kevin\\OneDrive\\Documents\\MSU\\2nd Sem\\CMSE 890 applied ML\\final submittion\\CMSE890\\temp.png")
-    # im = img_path
-    im.resize((224,224))
-    print(im.size)
+    im =  tf.keras.utils.load_img("C:\\Users\\kevin\\OneDrive\\Documents\\MSU\\2nd Sem\\CMSE 890 applied ML\\final submittion\\CMSE890\\temp.png",target_size=(224,224,3))
+    # print(im)
     im = tf.keras.utils.img_to_array(im)
     im = np.expand_dims(im, axis=0)
     return im
@@ -39,7 +38,7 @@ def predict_captions(image):
     start_word = ["<start>"]
     while True:
         par_caps = [word_2_indices[i] for i in start_word]
-        par_caps = sequence.pad_sequences([par_caps], maxlen=max_len, padding='post')
+        par_caps = tf.keras.utils.pad_sequences([par_caps], maxlen=max_len, padding='post')
         preds = model.predict([np.array([image]), np.array(par_caps)])
         word_pred = indices_2_word[np.argmax(preds[0])]
         start_word.append(word_pred)
@@ -74,9 +73,10 @@ if img:
     print("22222222222222")
     print(image)
     # path = saveImage(file)
-    # st.image(path)
+    st.image(image)
     resnet = tf.keras.applications.ResNet50(include_top=False,weights='imagenet',input_shape=(224,224,3),pooling='avg')
     #print(path)
     test_img = get_encoding(resnet, image)
     Argmax_Search = predict_captions(test_img)
-    print(Argmax_Search)
+    st.write()
+    st.write(Argmax_Search)
